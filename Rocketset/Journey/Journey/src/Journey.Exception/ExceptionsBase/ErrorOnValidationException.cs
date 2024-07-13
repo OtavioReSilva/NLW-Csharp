1b2c93aad5d010d84ca86f3a89c1f4c911f6e82c
Bowie <otavioreferino@gmail.com>
2024-07-13 01:50:28 -0300

    Final

---
+++ Rocketset/Journey/Journey/src/Journey.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace Journey.Exception.ExceptionsBase
+{
+    public class ErrorOnValidationException : JourneyException
+    {
+        private readonly IList<string> _erros;
+        public ErrorOnValidationException(IList<string> messages) : base(string.Empty)
+        {
+            _erros = messages;
+        }
+
+        public override IList<string> GetErrorMessages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override HttpStatusCode GetStatusCode()
+        {
+            return HttpStatusCode.BadRequest;
+        }
+    }
+}
